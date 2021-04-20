19fbecad64e9b07d3161b1db132e96396ee37f87
voidz8 <renzovermue@hotmail.com>
2021-04-20 10:17:28 +0200

    answers

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,9 @@ const product = {
   screenType: 'LED',
 }
 
+const {price, refreshRate} = product;
+console.log(price);
+console.log(refreshRate);
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -18,6 +21,9 @@ const productInformation = {
   smartTv: true,
 }
 
+const {type, brand} = productInformation.general;
+console.log(type);
+console.log(brand);
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
 const tvOptions = {
@@ -38,27 +44,41 @@ const tvOptions = {
   },
 };
 
+const {wifi, bluetooth} = tvOptions.options.connectivity.popular;
+console.log(wifi);
+console.log(bluetooth);
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
 
-
+const [first, second] = connectivity;
+console.log(first);
+console.log(second);
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
 
-
+const [firstNumber,, thirdNumber] = tvSizes;
+console.log(firstNumber);
+console.log(thirdNumber);
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
 function getCompanyDetails() {
   return {
     name: 'Novi Hogeschool',
-    adress: 'Zonnebaan 9, Utrecht',
+    address: 'Zonnebaan 9, Utrecht',
   }
 }
 
+const {name, address} = getCompanyDetails();
+console.log(name);
+console.log(address);
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+const [firstSentence, lastSentence] = getDetails('Renzo', 25);
+console.log(firstSentence);
+console.log(lastSentence);
